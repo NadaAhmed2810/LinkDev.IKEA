cb0c5917a27593df94eb8a22de0d557f106a3547
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-03-08 20:34:37 +0200

    Department Moduel

---
+++ LinkDev.IKEA.DAL/Persistance/Data/Configurations/Departments/DepartmentConfigurations.cs
@@ -0,0 +1,31 @@
+﻿using LinkDev.IKEA.DAL.Entities.Departments;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistance.Data.Configurations.Departments
+{
+    internal class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10,10);
+            builder.Property(D => D.Name).HasColumnType("varchar(100)");
+            builder.Property(D => D.Code).HasColumnType("varchar(10)");
+            builder.Property(D => D.Description).HasColumnType("varchar(100)");
+            builder.Property(D => D.CreatedBy).HasColumnType("varchar(100)");
+            builder.Property(D => D.LastModifiedBy).HasColumnType("varchar(100)");
+            builder.Property(D => D.LastModifiedOn).HasDefaultValueSql("GetUTCDate()");
+            builder.Property(D => D.CreatedOn).HasComputedColumnSql("GetUTCDate()");
+
+
+
+
+
+        }
+    }
+}
