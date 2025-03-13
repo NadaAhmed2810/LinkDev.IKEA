29dcb41ffcb1361cd485b9eba504723506900cde
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-03-13 23:15:22 +0200

    Department-Controller-Details

--- LinkDev.IKEA.PL/Controllers/DepartmentController.cs
+++ LinkDev.IKEA.PL/Controllers/DepartmentController.cs
@@ -16,10 +16,11 @@ namespace LinkDev.IKEA.PL.Controllers
             _departmentService = departmentService;
         }
         #endregion
+        #region Index
         [HttpGet]//baseUrl/Department/Index
         public IActionResult Index()
         {
-            var departments= _departmentService.GetDepartments();
+            var departments = _departmentService.GetDepartments();
             var DepartmentViewModel = departments.Select(department => new DepartmentViewModel()
             {
                 Id = department.Id,
@@ -30,5 +31,33 @@ namespace LinkDev.IKEA.PL.Controllers
 
             return View(DepartmentViewModel);
         }
+        #endregion
+        #region Details
+        [HttpGet] //Get :BaseUrl/Department/Details/id
+        public IActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+                return BadRequest();
+            var department = _departmentService.GetDepartmentById(id.Value);
+            if (department is null)
+                return NotFound();
+
+            var DepartmentDetailsView = new DepartmentDetailsView()
+            {
+                Id = department.Id,
+                Name = department.Name,
+                Code = department.Code,
+                CreationDate = department.CreationDate,
+                CreatedBy = department.CreatedBy,
+                CreatedOn = department.CreatedOn,
+                LastModifiedBy = department.LastModifiedBy,
+                LastModifiedOn = department.LastModifiedOn
+
+            };
+
+            return View(department);
+        }
+
+        #endregion
     }
 }
--- LinkDev.IKEA.PL/ViewModels/Departments/DepartmentViewModel.cs
+++ LinkDev.IKEA.PL/ViewModels/Departments/DepartmentViewModel.cs
@@ -5,10 +5,11 @@ namespace LinkDev.IKEA.PL.ViewModels.Departments
     public class DepartmentViewModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Code { get; set; }
+        public required string Name { get; set; }
+        public required string Code { get; set; }
         [Display(Name ="Date of Creation") ]
         public DateOnly CreationDate { get; set; }
+
 
     }
 }
