4bd7a3d4d1e68b42cbb0332f8af9e26aaba99b2b
NadaAhmed <nwrabsywny534@gmail.com>
2025-05-02 07:38:53 +0300

    Generic Repository

--- LinkDev.IKEA.DAL/Contracts/Repositories/IEmployeeRepository.cs
+++ LinkDev.IKEA.DAL/Contracts/Repositories/IEmployeeRepository.cs
@@ -1,15 +1,15 @@
-﻿using LinkDev.IKEA.DAL.Entities.Departments;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinkDev.IKEA.DAL.Entities.Employees;
 
 namespace LinkDev.IKEA.DAL.Contracts.Repositories
 {
-    public interface IDepartmentRepository : IBaseRepository<Department, int>
+    public interface IEmployeeRepository:IBaseRepository<Employee, int>
     {
-        
-
+      
+    
     }
 }
