5873070ee6f66b3e34047bc5c42c3b852c55bdf3
NadaAhmed <nwrabsywny534@gmail.com>
2025-05-20 19:17:05 +0300

    EmployeeController

--- LinkDev.IKEA.DAL/Common/Enums/Gender.cs
+++ LinkDev.IKEA.DAL/Common/Enums/Gender.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Microsoft.VisualBasic.FileIO;
+
 
 namespace LinkDev.IKEA.DAL.Common.Enums
 {
     public enum Gender
     {
         Male=1,
+        Female=2,
+
     }
 }
---
+++ LinkDev.IKEA.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using LinkDev.IKEA.BLL.Services.Employees;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkDev.IKEA.PL.Controllers
+{
+    public class EmployeeController:Controller
+    {
+        private readonly ILogger<EmployeeController> logger;
+        #region Services
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(ILogger<EmployeeController> logger, IEmployeeService employeeService)
+        {
+            this.logger = logger;
+            _employeeService = employeeService;
+        }
+        #endregion
+
+    }
+}
